3246b8ca892afd0ba7865045317e22c37b1c3924
milab 5 <milab@iMac-de-milab.local>
2017-07-08 10:51:25 -0500

    First commit to pets app

---
+++ MyPets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPets
+//
+//  Created by milab 5 on 08/07/17.
+//  Copyright © 2017 diegohdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
